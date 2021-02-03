673ac7f01f4e422e9cfd79526c1a5d8b2e4cfdf9
Dawud <dabdulkerim04@gmail.com>
2021-02-03 09:57:11 +0000

    second commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/**
+ * main - main block
+ * Return: 0
+ */
 int main(void)
-{printf("Size of a char: %i byte(s)\n", sizeof(char));
+{
+printf("Size of a char: %i byte(s)\n", sizeof(char));
 printif("Size of an int: %i byte(s)\n", sizeof(int));
 printf("Size of a long int: %i byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %i byte(s)", sizeof(long long int));

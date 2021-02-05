32b974a7f1f8ae70044e92c6640b0c7f2467c2c2
Dawud <dabdulkerim04@gmail.com>
2021-02-05 18:04:01 +0000

    advanced 101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,26 +10,33 @@ int main(void)
 {
 int c;
 int d;
-int e=0;
+int e = 0;
 while (e < 10)
 {
-d=0;
-while(d < 10)
-{c = 0;
+d = 0;
+while (d < 10)
+{
+c = 0;
 while (c < 10)
 {
-if (c != d && d !=e && e < d && d < c)
-{putchar('0' + e);
+if (c != d && d != e && e < d && d < c)
+{
+putchar('0' + e);
 putchar('0' + d);
 putchar('0' + c);
-if (c +d +e != 9 + 8 + 7)
+if (c + d + e != 9 + 8 + 7)
 {
 putchar(',');
-putchar(' ');}
+putchar(' ');
 }
-}c++;
-}d++;
-}e++;
-}putchar('\n');
+}
+}
+c++;
+}
+d++;
+}
+e++;
+}
+putchar('\n');
 return (0);
 }

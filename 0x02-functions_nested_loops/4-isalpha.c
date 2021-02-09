efe16080867a283cd8e2b8f175b37097292860eb
Dawud <dabdulkerim04@gmail.com>
2021-02-09 10:52:42 +0000

    comment added

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,4 +1,10 @@
 #include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
 int _isalpha(int c)
 {
 return ((c >= 97 && c <= 122) || (c >= 65 && c <= 90));

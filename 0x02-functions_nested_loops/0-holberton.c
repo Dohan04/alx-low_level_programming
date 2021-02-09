eebddbd6c7d00262911a9795599a01ef6bf74b5a
Dawud <dabdulkerim04@gmail.com>
2021-02-09 09:35:56 +0000

    first commit on   0x02

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char c[10] = "Holberton";
+int i = 0;
+while (i < 9)
+{
+_putchaar(c[i]);
+i++;
+}
+_putchar('\n');
+return (0);
+}

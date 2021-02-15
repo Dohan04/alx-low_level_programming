8975232760a0e20593f0a6ed996d9e647db837e4
Dawud <dabdulkerim04@gmail.com>
2021-02-15 06:11:20 +0000

    the first 4 qustions

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "holberton.c"
+
+/**
+ * reset_to_98 - reset argument to 98
+ *
+ * @n: Pointer
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * swap_int - swap variable values
+ *
+ * @a: pointer1
+ *
+ * @b: pointer 2
+ *
+ * Return:void
+ */
+void swap_int(int *a, int *b)
+{
+*a += *b;
+*b = *a - *b;
+*a = *a - *b;
+}
